5564c48439c9997745f70c508f1d89c32669ee56
xiaolixu <xiaolixu@cn.ibm.com>
2017-03-07 11:46:33 +0800

    nlu init

--- server.py
+++ server.py
@@ -1,21 +1,34 @@
+# coding: utf-8
+
+# In[4]:
+
+import sys
 import os
-try:
-  from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
-  from SocketServer import TCPServer as Server
-except ImportError:
-  from http.server import SimpleHTTPRequestHandler as Handler
-  from http.server import HTTPServer as Server
+import json
+from flask import Flask
+from flask import jsonify
 
-# Read port selected by the cloud for our application
-PORT = int(os.getenv('PORT', 8000))
-# Change current directory to avoid exposure of control files
-os.chdir('static')
+sys.path.append(os.path.join(os.getcwd(),'..'))
+import watson_developer_cloud
+import watson_developer_cloud.natural_language_understanding.features.v1 as \
+    features
 
-httpd = Server(("", PORT), Handler)
-try:
-  print("Start serving at port %i" % PORT)
-  httpd.serve_forever()
-except KeyboardInterrupt:
-  pass
-httpd.server_close()
 
+app = Flask("NLU App")
+
+nlu = watson_developer_cloud.NaturalLanguageUnderstandingV1(
+        version='2017-02-27',
+        username=os.getenv('NATURAL_LANGUAGE_UNDERSTANDING_USERNAME'),
+        password=os.getenv('NATURAL_LANGUAGE_UNDERSTANDING_PASSWORD'))
+
+@app.route("/")
+def eval_default():
+        response = nlu.analyze(
+                text='Bruce Banner is the Hulk and Bruce Wayne is BATMAN! '
+                'Superman fears not Banner, but Wayne.',
+                features=[features.Entities(), features.Keywords()])
+        return jsonify(response)
+ 
+ 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=True,port=int(os.getenv('PORT',8080)))
